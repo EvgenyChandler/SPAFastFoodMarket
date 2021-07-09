06434e4639de1139796b9b54afa9148fde757a9e
Philippov <evgeny.chandler@gmail.com>
2021-07-09 12:36:55 +0300

    add AtlasDB

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { dbConnectionURL, options, DB_NAME } = require('./dbConfig');
 
 function dbConnect() {
-  mongoose.connect(dbConnectionURL, options, (err) => {
+  mongoose.connect('mongodb+srv://fastfoodmarket:fastfood123@cluster0.gxpjo.mongodb.net/fastFoodMarketDB?retryWrites=true&w=majority', options, (err) => {
     if (err) console.log(err);
     console.log(
       `Database ("${DB_NAME}") has been connected: ${dbConnectionURL}`,
